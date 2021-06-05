7c98ddfffe6d4ca1311250d1395b39768fb3fe5e
micha <mkipp@stud.hs-heilbronn.de>
2021-06-05 15:22:09 +0100

    Einfache GUI mit MainMethode

--- src/mandelbrot/Main.java
+++ src/mandelbrot/Main.java
@@ -3,6 +3,7 @@ package mandelbrot;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	MandelbrotSet mbs = new MandelbrotSet();
+
     }
 }
--- src/mandelbrot/MandelbrotSet.java
+++ src/mandelbrot/MandelbrotSet.java
@@ -1,6 +1,30 @@
 package mandelbrot;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MandelbrotSet {
+    private int height = 500;
+    private int width = 500;
+    public MandelbrotSet(){
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel contentPane = new JPanel();
+        contentPane.setLayout(new BorderLayout());
+
+        Canvas canvas = new Canvas();
+        contentPane.add(canvas);
+        frame.add(contentPane);
+        frame.setVisible(true);
+        frame.setResizable(false);
+
+        for(int x = 0; x < width;x++){
+
+        }
+
+    }
 
     public static void calculateMandelbrotSet(float xVal, float yVal){
         
