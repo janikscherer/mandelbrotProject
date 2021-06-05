edb63a8a56c1851ae61ed373c4538c1126040f42
micha <mkipp@stud.hs-heilbronn.de>
2021-06-05 18:36:04 +0100

    Fast funktionierend

--- src/mandelbrot/MandelbrotSet.java
+++ src/mandelbrot/MandelbrotSet.java
@@ -10,65 +10,93 @@ import java.io.IOException;
 public class MandelbrotSet {
     private int height = 500;
     private int width = 500;
-    public MandelbrotSet(){
-        File f = null;
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+
+    public MandelbrotSet() {
+
+    }
+
+    public void createAndShowGUI() {
         JFrame frame = new JFrame();
-        frame.setSize(500, 500);
+        frame.setBackground(Color.black);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel contentPane = new JPanel();
-        contentPane.setLayout(new BorderLayout());
-
-        Canvas canvas = new Canvas();
-        contentPane.add(canvas);
-        frame.add(contentPane);
+        frame.getContentPane().setLayout(new GridLayout(1, 1));
+        BufferedImage image = createImage(500, 500);
+        ImagePanel imagePanel = new ImagePanel(image);
+        frame.getContentPane().add(imagePanel);
+        frame.pack();
         frame.setVisible(true);
-        frame.setResizable(false);
+    }
 
-        for(int x = 0; x < width;x++){
-            for(int y = 0; y < height; y++){
+    public BufferedImage createImage(int width, int height) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 int color = calculateMandelbrotSet(x, y); // Calculate for each pixel
                 image.setRGB(x, y, color);
             }
         }
-
-        try{
-            f= new File("mandelbrot.png");
-            ImageIO.write(image, "png", f);
-        }
-        catch (IOException ex){
-            ex.printStackTrace();
-        }
+        return image;
     }
 
 
-    public int calculateMandelbrotSet(float xVal, float yVal){
-        float a = xVal/width*4-2;
-        float b = yVal/height*4-2;
-        final float originalA = a;
-        final float originalB = b;
+        public int calculateMandelbrotSet(float xVal, float yVal){
+            float a = xVal/width*4-2;
+            float b = yVal/height*4-2;
+            final float originalA = a;
+            final float originalB = b;
 
-        int maxIterations = 100;
+            int maxIterations = 100;
 
-        float z = 0;
+            float z = 0;
 
-        int iterations = 0;
-        for(; iterations<maxIterations; iterations++){
-            float newA = a*a - b*b;
-            float newB = 2*a*b;
-            a= newA + originalA;
-            b= newB + originalB;
+            int iterations = 0;
+            for(; iterations<maxIterations; iterations++){
+                float newA = a*a - b*b;
+                float newB = 2*a*b;
 
-            if(Math.abs(a+b)>16){
-                break;
+                a= newA + originalA;
+                b= newB + originalB;
+
+                if(Math.abs(a+b)>16){
+                    break;
+                }
             }
-        }
 
-        if(iterations == maxIterations-1){
-            return 0;
-        }
-        else return 255;
+            if(iterations == maxIterations-85){
+                return 0;
+            }
+            else return 255;
+
 
     }
+
+
+    static class ImagePanel extends JPanel
+    {
+        private final BufferedImage image;
+        ImagePanel(BufferedImage image)
+        {
+            this.image = image;
+        }
+
+        @Override
+        public Dimension getPreferredSize()
+        {
+            if (super.isPreferredSizeSet())
+            {
+                return super.getPreferredSize();
+            }
+            return new Dimension(image.getWidth(), image.getHeight());
+        }
+
+        @Override
+        protected void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+            g.drawImage(image, 0, 0, null);
+        }
+    }
+
 }
+
