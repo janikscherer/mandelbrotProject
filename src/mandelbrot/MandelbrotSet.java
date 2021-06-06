cc9f9cad545d1cac335facd0de09737b02a5a941
micha <mkipp@stud.hs-heilbronn.de>
2021-06-06 01:33:11 +0100

    added visualisation version 3

--- src/mandelbrot/MandelbrotSet.java
+++ src/mandelbrot/MandelbrotSet.java
@@ -25,6 +25,7 @@ public class MandelbrotSet {
         ImagePanel imagePanel = new ImagePanel(image);
         frame.getContentPane().add(imagePanel);
         frame.pack();
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 
@@ -40,9 +41,9 @@ public class MandelbrotSet {
     }
 
 
-        public int calculateMandelbrotSet(float xVal, float yVal){
-            float a = xVal/width*4-2;
-            float b = yVal/height*4-2;
+        public int calculateMandelbrotSet(float xVal, float yVal) {
+            float a = xVal / width * 4 - 2;
+            float b = yVal / height * 4 - 2;
             final float originalA = a;
             final float originalB = b;
 
@@ -51,14 +52,14 @@ public class MandelbrotSet {
             float z = 0;
 
             int iterations = 0;
-            for(; iterations<maxIterations; iterations++){
-                float newA = a*a - b*b;
-                float newB = 2*a*b;
+            for (; iterations < maxIterations; iterations++) {
+                float newA = a * a - b * b;
+                float newB = 2 * a * b;
 
-                a= newA + originalA;
-                b= newB + originalB;
+                a = newA + originalA;
+                b = newB + originalB;
 
-                if(Math.abs(a*a+b*b+2*a*b)>10000000){
+                if (Math.abs(a * a + b * b + 2 * a * b) > 10000000) {
                     break;
                 }
             }
@@ -71,20 +72,35 @@ public class MandelbrotSet {
             */
 
             //Version 2 (Mitte gefuellt außen abgestuft)
-            if(iterations == maxIterations) {
+            if (iterations == maxIterations) {
                 return 0;
-            } else if(iterations == maxIterations-80) {
+            } else if (iterations == maxIterations - 80) {
                 return 180;
-            } else if(iterations == maxIterations-79) {
+            } else if (iterations == maxIterations - 79) {
                 return 135;
-            } else if(iterations == maxIterations-78) {
+            } else if (iterations == maxIterations - 78) {
                 return 90;
-            } else if(iterations == maxIterations-77) {
+            } else if (iterations == maxIterations - 77) {
                 return 45;
-            } else if(iterations <= maxIterations-76) {
+            } else if (iterations <= maxIterations - 76) {
                 return 0;
             } else return 255;
 
+        /*
+            //Version 3 (Mitte gefuellt außen abgestuft in unterschiedlichen Farben)
+            if (iterations > 9*maxIterations/10) return Color.BLACK.getRGB();
+            else if (iterations > 8*maxIterations/10)return Color.GRAY.getRGB();
+            else if (iterations > 7*maxIterations/10) return Color.MAGENTA.getRGB();
+            else if (iterations > 6*maxIterations/10) return Color.CYAN.getRGB();
+            else if (iterations > 5*maxIterations/10) return Color.BLUE.getRGB();
+            else if (iterations > 4*maxIterations/10) return Color.GREEN.getRGB();
+            else if (iterations > 3*maxIterations/10) return  Color.YELLOW.getRGB();
+            else if (iterations > 2*maxIterations/10) return Color.ORANGE.getRGB();
+            else if (iterations > 1*maxIterations/10) return Color.RED.getRGB();
+            else return 0;
+
+
+         */
     }
 
 
@@ -103,7 +119,7 @@ public class MandelbrotSet {
             {
                 return super.getPreferredSize();
             }
-            return new Dimension(image.getWidth(), image.getHeight());
+            return new Dimension(image.getWidth(), image.getHeight()); //encapsulates the width and height of a component (in integer precision) in a single object
         }
 
         @Override
@@ -111,6 +127,7 @@ public class MandelbrotSet {
         {
             super.paintComponent(g);
             g.drawImage(image, 0, 0, null);
+
         }
     }
 
