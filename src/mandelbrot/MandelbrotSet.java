97fbfb628b0cc2c9da429c44fb0285709e3d9ee7
janikscherer <schererjanik@gmail.com>
2021-06-06 10:13:32 +0200

    improved color gradient in calculateMandelbrotSet so it utilizes the whole color spectrum up to Integer MaxValue. Removed the other coloring versions for easier readability.

--- src/mandelbrot/MandelbrotSet.java
+++ src/mandelbrot/MandelbrotSet.java
@@ -41,97 +41,51 @@ public class MandelbrotSet {
     }
 
 
-        public int calculateMandelbrotSet(float xVal, float yVal) {
-            float a = xVal / width * 4 - 2;
-            float b = yVal / height * 4 - 2;
-            final float originalA = a;
-            final float originalB = b;
+    public int calculateMandelbrotSet(float xVal, float yVal) {
+        float a = xVal / width * 4 - 2;
+        float b = yVal / height * 4 - 2;
+        final float originalA = a;
+        final float originalB = b;
+        int maxIterations = 90;
 
-            int maxIterations = 90;
+        int iterations = 0;
+        for (; iterations < maxIterations; iterations++) {
+            float newA = a * a - b * b;
+            float newB = 2 * a * b;
 
-            float z = 0;
+            a = newA + originalA;
+            b = newB + originalB;
 
-            int iterations = 0;
-            for (; iterations < maxIterations; iterations++) {
-                float newA = a * a - b * b;
-                float newB = 2 * a * b;
-
-                a = newA + originalA;
-                b = newB + originalB;
-
-                if (Math.abs(a * a + b * b + 2 * a * b) > 10000000) {
-                    break;
-                }
+            if (Math.abs(a * a + b * b + 2 * a * b) > 10000000) {
+                break;
             }
+        }
 
-            //Version 1 (Schwarz gefuellt ohne Abstufung)
-            /*
-            if(iterations == maxIterations) {
-                return 0;
-            } else return 255;
-            */
-
-/*            //Version 2 (Mitte gefuellt außen abgestuft)
-            if (iterations == maxIterations) {
-                return 0;
-            } else if (iterations == maxIterations - 80) {
-                return 180;
-            } else if (iterations == maxIterations - 79) {
-                return 135;
-            } else if (iterations == maxIterations - 78) {
-                return 90;
-            } else if (iterations == maxIterations - 77) {
-                return 45;
-            } else if (iterations <= maxIterations - 76) {
-                return 0;
-            } else return 255;*/
-
-/*            //Version 3 (Mitte gefuellt außen abgestuft in unterschiedlichen Farben)
-            if (iterations > 9*maxIterations/10) return Color.BLACK.getRGB();
-            else if (iterations > 8*maxIterations/10)return Color.GRAY.getRGB();
-            else if (iterations > 7*maxIterations/10) return Color.MAGENTA.getRGB();
-            else if (iterations > 6*maxIterations/10) return Color.CYAN.getRGB();
-            else if (iterations > 5*maxIterations/10) return Color.BLUE.getRGB();
-            else if (iterations > 4*maxIterations/10) return Color.GREEN.getRGB();
-            else if (iterations > 3*maxIterations/10) return  Color.YELLOW.getRGB();
-            else if (iterations > 2*maxIterations/10) return Color.ORANGE.getRGB();
-            else if (iterations > 1*maxIterations/10) return Color.RED.getRGB();
-            else return 0;*/
-
-            // Version 4 (color gradient: Keine klare Abstufung mehr sondern Farbverlauf)
-            if(iterations == maxIterations) {
-                return 0;
-            }
-            else return  (iterations*(Integer.MAX_VALUE/(maxIterations)));
-
-
+        if (iterations == maxIterations) {
+            return 0;
+        } else return (iterations * (Integer.MAX_VALUE / (maxIterations)));
     }
 
 
-    static class ImagePanel extends JPanel
-    {
+    static class ImagePanel extends JPanel {
         private final BufferedImage image;
-        ImagePanel(BufferedImage image)
-        {
+
+        ImagePanel(BufferedImage image) {
             this.image = image;
         }
 
         @Override
-        public Dimension getPreferredSize()
-        {
-            if (super.isPreferredSizeSet())
-            {
+        public Dimension getPreferredSize() {
+            if (super.isPreferredSizeSet()) {
                 return super.getPreferredSize();
             }
             return new Dimension(image.getWidth(), image.getHeight()); //encapsulates the width and height of a component (in integer precision) in a single object
         }
 
         @Override
-        protected void paintComponent(Graphics g)
-        {
+        protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.drawImage(image, 0, 0, null);
-
         }
     }
 
